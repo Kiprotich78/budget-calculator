5cdc7aba20cfab79609087eb5578f6a07b3486ba
Kiprotich <shadrackr057@gmail.com>
2023-01-05 14:43:58 +0300

    added an input form

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Item from './ExpenseItem.js'
-const ExpenseList = () => {
+import {MdDelete} from 'react-icons/md'
+
+const ExpenseList = ({expenses}) => {
   return (
-      <div> Expense List
-          < Item/>
-    </div>
+    <> 
+      <ul className='list'>
+        {expenses.map((expense) => {
+          return <Item key={expense.id} expense={expense}/> 
+        })}
+      </ul>
+      {expenses.length > 0 && <button className="btn">
+        Clear Expenses
+        <MdDelete className='btn-icon'/>
+      </button>}
+    </>
   )
 }
 
